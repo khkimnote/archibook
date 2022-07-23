66516b275842e0407dc88b6494b3bb6e7f656072
DESKTOP-2O4EC69\rlawl <rlawlstjr543@naver.com>
2022-07-23 22:52:23 +0900

    [Ogura] findId.html,findPw.html 생성

---
+++ src/main/java/com/example/archibook/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.archibook.controller;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+@RequestMapping("/member/*")
+@RequiredArgsConstructor
+public class MemberController {
+
+
+    @GetMapping("login")
+    public void login(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(); //주석 :20220723 추후 커맨트 예정
+        model.addAttribute("memberId", session.getAttribute("memberId")); //주석 :20220723 추후 커맨트 예정
+    }
+
+    @GetMapping("join")
+    public void join(HttpServletRequest request, Model model){
+        HttpSession session = request.getSession(); //주석 :20220723 추후 커맨트 예정
+        model.addAttribute("memberId", session.getAttribute("memberId")); //주석 :20220723 추후 커맨트 예정
+    }
+
+    @GetMapping("findId")
+    public String findId(){
+        return "member/findId";
+
+    }
+
+
+}
+
