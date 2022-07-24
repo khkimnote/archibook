f2601ec20fafbccddadb32d60c38b9ea9b09a1f1
Molaaaaaaa <jeongjh4268@naver.com>
2022-07-24 12:05:56 +0900

    [Junho] 고객센터, QnA 수정

--- src/main/java/com/example/archibook/ArchibookApplication.java
+++ src/main/java/com/example/archibook/ArchibookApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ArchibookApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ArchibookApplication.class, args);
     }
-
 }
